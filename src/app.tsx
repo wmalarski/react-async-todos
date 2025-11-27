05414dbf093161b5803be1aaa59ce5c9ed341b8b
Wojtek Malarski <w.malarski@gmail.com>
2025-11-27 23:02:00 +0100

    cleans up example

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import "./app.css";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,26 +8,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" rel="noopener" target="_blank">
-          <img alt="Vite logo" className="logo" src={viteLogo} />
-        </a>
-        <a href="https://react.dev" rel="noopener" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
-        <a
-          href="https://workers.cloudflare.com/"
-          rel="noopener"
-          target="_blank"
-        >
-          <img
-            alt="Cloudflare logo"
-            className="logo cloudflare"
-            src={cloudflareLogo}
-          />
-        </a>
-      </div>
-      <h1>Vite + React + Cloudflare</h1>
-      <div className="card">
         <Button
           aria-label="increment"
           onClick={() => setCount((count) => count + 1)}
@@ -43,7 +19,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <div className="card">
+      <div>
         <Button
           aria-label="get name"
           onClick={() => {
@@ -55,13 +31,7 @@ function App() {
         >
           Name from API is: {name}
         </Button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
