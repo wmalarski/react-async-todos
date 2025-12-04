723802c62915b9741468284f473d103c0716ae1d
Wojtek Malarski <w.malarski@gmail.com>
2025-12-04 13:36:47 +0100

    adds bookmark tags

--- src/modules/bookmarks/bookmark-list.tsx
+++ src/modules/bookmarks/bookmark-list.tsx
@@ -1,9 +1,15 @@
-import { use } from "react";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
+import { use, useMemo } from "react";
+
+import type { TagOutput } from "../tags/services/actions";
+import { useTagsContext } from "../tags/tags-provider";
 import {
   type BookmarksQueryPage,
   useBookmarksContext,
 } from "./bookmarks-provider";
+import type { BookmarkOutput } from "./services/actions";
 import { BOOKMARK_STATUSES } from "./services/constansts";
 import type { BookmarkStatus } from "./services/types";
 
@@ -28,8 +34,12 @@ const BookmarkColumn = ({ status }: BookmarkColumnProps) => {
 
   return (
     <div>
-      {pages.map((page) => (
-        <BookmarkPage key={`page-${page.args.page}`} page={page} />
+      {pages.map((page, index) => (
+        <BookmarkPage
+          isLastPage={index === pages.length - 1}
+          key={`page-${page.args.page}`}
+          page={page}
+        />
       ))}
     </div>
   );
@@ -37,10 +47,71 @@ const BookmarkColumn = ({ status }: BookmarkColumnProps) => {
 
 type BookmarkPageProps = {
   page: BookmarksQueryPage;
+  isLastPage: boolean;
 };
 
 const BookmarkPage = ({ page }: BookmarkPageProps) => {
   const bookmarks = use(page.promise);
 
-  return <pre>{JSON.stringify(bookmarks, null, 2)}</pre>;
+  return (
+    <>
+      {bookmarks.data.bookmarks.map((bookmark) => (
+        <BookmarkCard bookmark={bookmark} key={bookmark.id} />
+      ))}
+    </>
+  );
+};
+
+type BookmarkCardProps = {
+  bookmark: BookmarkOutput;
+};
+
+const BookmarkCard = ({ bookmark }: BookmarkCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <BookmarkTags bookmark={bookmark} />
+        <CardTitle>{bookmark.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <pre>{JSON.stringify(bookmark, null, 2)}</pre>
+      </CardContent>
+    </Card>
+  );
+};
+
+type BookmarkTagsProps = {
+  bookmark: BookmarkOutput;
+};
+
+const BookmarkTags = ({ bookmark }: BookmarkTagsProps) => {
+  const tagsContext = useTagsContext();
+  const tags = use(tagsContext.promise);
+
+  const matchedTags = useMemo(() => {
+    const tagsMap = new Map<string, TagOutput>();
+
+    tags?.forEach((tag) => {
+      tagsMap.set(tag.id, tag);
+    });
+
+    const matched: TagOutput[] = [];
+
+    bookmark.tags.forEach((tag) => {
+      const match = tagsMap.get(tag.tagId);
+      if (match) {
+        matched.push(match);
+      }
+    });
+
+    return matched;
+  }, [tags?.forEach, bookmark.tags.forEach]);
+
+  return (
+    <div className="flex flex-wrap gap-1">
+      {matchedTags.map((tag) => (
+        <Badge key={tag.id}>{tag.name}</Badge>
+      ))}
+    </div>
+  );
 };
